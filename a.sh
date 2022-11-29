12bba561776253301ddf39d6cc29b369e63d3e5a
EC2 Default User <ec2-user@ip-172-31-1-148.ap-south-1.compute.internal>
2022-11-29 17:32:36 +0000

    committed again

--- a.sh
+++ a.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/bash
-a=4
+a=7
 b=5
 echo "value of a is $a and that of b is $b"
+
+function add()
+{
+	echo "This is add func"
+	echo "sum of a and b is = ($a+$b)"
+}
+function sub()
+{
+	echo "THis is sub func"
+	echo "sub of a and b is = ($a-$b)"
+}
+add
+sub
